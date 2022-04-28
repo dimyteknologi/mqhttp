7a5672b11a22f9c4316fa30c3d15b38a015cc17c
Ilham Bahari <muhari07@gmail.com>
2022-04-28 11:21:00 +0700

    init

---
+++ src/MqHttp.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Qoligo;
+
+use GuzzleHttp\Client as HttpClient;
+
+class MqHttp {
+
+    public function __construct()
+    {
+        $this->connection = new HttpClient();
+    }
+
+    public function publishMessage($event, $payload, $arguments = [])
+    {
+      try {
+        $this->connection->request('POST', config('app.config_base_url') . config('app.config_producer_url'), [
+            'form_params' => [
+                'event' => $event,
+                'payload' => $payload,
+                'arguments' => $arguments
+            ],
+            \GuzzleHttp\RequestOptions::HEADERS => [
+                'Authorization' => config('app.authorization')
+            ]
+        ]);
+      } catch (\Exception $e) {
+        throw $e;
+      }
+    }
+}
